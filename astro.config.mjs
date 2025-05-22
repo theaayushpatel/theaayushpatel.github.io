0a02ffc505070c19ab16460d2e8c4930e3309e5f
theaayushpatel <aayushp9326@gmail.com>
2025-05-22 10:24:26 +0530

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://theaayushpatel.github.io',
+  base: '/theaayushpatel.github.io',
+})
