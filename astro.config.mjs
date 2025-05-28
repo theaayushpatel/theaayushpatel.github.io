8654c50e24a50c17ff5509e23f85a3945305f453
theaayushpatel <aayushp9326@gmail.com>
2025-05-28 11:28:14 +0530

    Gitpages not shown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://theaayushpatel.github.io',
   base: '/',
   output: 'static',
+  integrations: [expressiveCode()],
 })
